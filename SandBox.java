7899398b03012096b981d299cd2abb1bae7799dc
Deema <deema2002@live.ca>
2020-01-27 11:15:01 -0500

    calculated my age

--- SandBox.java
+++ SandBox.java
@@ -2,7 +2,13 @@ public class SandBox {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("/n This is my sandbox project!");
+        System.out.println("\n This is my sandbox project!");
+
+        final short yearOfBirth = 2002;
+        short currentYear = 2020;
+        short age;
+        age = (short)(currentYear - yearOfBirth);
+        System.out.println("I turn " + age + " this year!");
     }
 
 }
